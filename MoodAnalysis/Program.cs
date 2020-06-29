5c3ec0ff7e0d6439972e10cc669b3009412e5611
Abhishek Bodake <rishibodake@gmail.com>
2020-06-29 17:08:17 +0530

    Added:.gitignore file for project

---
+++ MoodAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
