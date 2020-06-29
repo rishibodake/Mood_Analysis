a7261b39c6646e1b34c27b9c55c95e08ef9967d8
Abhishek Bodake <rishibodake@gmail.com>
2020-06-29 18:17:27 +0530

    Refactored:Method To Handle Null Value When Provided As Message

--- MoodAnalysis/MoodAnalyser.cs
+++ MoodAnalysis/MoodAnalyser.cs
@@ -4,6 +4,9 @@ namespace MoodAnalysis
 {
     public class MoodAnalyser
     {
+        static void Main(string[] args)
+        {
+        }
         public MoodAnalyser()
         {
 
@@ -11,18 +14,26 @@ namespace MoodAnalysis
 
         public string AnalyseMood(string message)
         {
-            if (message.ToLower().Contains("sad"))
+            try
             {
-                return "SAD";
+                if (message.ToLower().Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else if (message.ToLower().Contains("happy"))
+                {
+                    return "HAPPY";
+                }
+                else
+                {
+                    return "Not Sure About Mood";
+                }
             }
-            else if(message.ToLower().Contains("happy"))
+            catch (NullReferenceException e)
             {
                 return "HAPPY";
             }
-            else
-            {
-                return "Not Sure About Mood";
-            }
+            
         }
     }
 }
